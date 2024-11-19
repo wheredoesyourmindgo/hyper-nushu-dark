062d05da551f94a206f8f983da122f642a4d2bea
Abe Hendricks <ahendricks@pcwa.net>
2024-11-19 08:30:14 -0800

    Setup theme.

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 module.exports.decorateConfig = (config) => {
   const nushuDarkColors = {
-    backgroundColor: '#F8F9FA', // Background
-    foregroundColor: '#333333', // Foreground (Text)
-    cursorColor: '#FF5733',     // Cursor
-    borderColor: '#CCCCCC',     // Border
-    selectionColor: 'rgba(255, 87, 51, 0.3)', // Selection (rgba)
+    foregroundColor: "#d7cfc7",
+    backgroundColor: "#25211d",
+    selectionColor: "#4a3b23",
+    cursorColor: "#3c7d3f",
+    borderColor: "#3f3b36e6",
     colors: {
-      black: '#1E1E1E',
-      red: '#FF5733',
-      green: '#28A745',
-      yellow: '#FFC107',
-      blue: '#007BFF',
-      magenta: '#6F42C1',
-      cyan: '#17A2B8',
-      white: '#F8F9FA',
-      lightBlack: '#555555',
-      lightRed: '#FF7961',
-      lightGreen: '#33D17A',
-      lightYellow: '#FFD54F',
-      lightBlue: '#409EFF',
-      lightMagenta: '#AF69EF',
-      lightCyan: '#20C997',
-      lightWhite: '#FFFFFF',
-    }
+      black: "#484f58",
+      red: "#ff7b72",
+      green: "#3fb950",
+      yellow: "#d29922",
+      blue: "#58a6ff",
+      magenta: "#bc8cff",
+      cyan: "#39c5cf",
+      white: "#b1bac4",
+      lightBlack: "#6e7681",
+      lightRed: "#ffa198",
+      lightGreen: "#56d364",
+      lightYellow: "#e3b341",
+      lightBlue: "#79c0ff",
+      lightMagenta: "#d2a8ff",
+      lightCyan: "#56d4dd",
+      lightWhite: "#ffffff",
+    },
+    css: `
+      * {
+        text-rendering: optimizeLegibility;
+      }
+      .tab_tab {
+        color: #7d8590;
+        background-color: #2b2724;
+      }
+      .tab_tab.tab_active {
+        color: #d7cfc7;
+        background-color: #25211c;
+      },
+    `,
   };
 
   return Object.assign({}, config, nushuDarkColors);
